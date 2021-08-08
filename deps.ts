e801b263e2aef6dcf435d0b54cf860adaa34ef10
Carlos Oening <carlosoening@gmail.com>
2021-08-08 00:43:28 -0300

    added video on client background and refac

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 // Standard library dependencies
-export * as log from 'https://deno.land/std@0.103.0/log/mod.ts';
-export { join } from 'https://deno.land/std@0.103.0/path/mod.ts';
-export { BufReader } from 'https://deno.land/std@0.103.0/io/bufio.ts';
-export { parse } from 'https://deno.land/std@0.103.0/encoding/csv.ts';
+export * as log from 'https://deno.land/std@0.102.0/log/mod.ts';
+export { join } from 'https://deno.land/std@0.102.0/path/mod.ts';
+export { BufReader } from 'https://deno.land/std@0.102.0/io/bufio.ts';
+export { parse } from 'https://deno.land/std@0.102.0/encoding/csv.ts';
 
 // Third party dependencies
 export { 
